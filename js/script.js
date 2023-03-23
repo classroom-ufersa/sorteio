946101d963eb1f9ec47108583255fdb741cf7f7e
Rosana <rosanacibely@gmail.com>
2023-03-23 21:46:56 +0000

    refactor: refactored generatenumber

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function generateNumber() {
     var end = document.getElementById("end").value;
     var range = end - start + 1;
     var randomNumber = Math.floor(Math.random() * range) + parseInt(start);
-    document.getElementById("result").innerHTML = "O aluno sorteado foi:: " + randomNumber;
+    document.getElementById("result").innerHTML = "O aluno sorteado foi: " + randomNumber;
     return false;
 
 }
@@ -12,3 +12,4 @@ document.getElementById("raffle-form").addEventListener("submit", function(event
     event.preventDefault();
     generateNumber();
 });
+
